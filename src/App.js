7bdf3341f9fed74fe58cb1c3ac5a9a72238fef99
isalu18 <luke_shakz@hotmail.com>
2021-03-21 13:49:39 -0700

    Creating second form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pregunta from './components/Pregunta'
+import Formulario from './components/Formulario'
 
 function App() {
+
+  //Definir el State
+  const [ presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarRestante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+
   return (
     <div className="container">
       <header>
@@ -9,7 +16,26 @@ function App() {
         <h1>Gasto semanal</h1>
 
         <div className="contenido-principal contenido">
-          <Pregunta/>
+
+          { mostrarpregunta ? 
+            (<Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+            />
+            ) : 
+            (<div className="row">
+                <div className="one-half column">
+                  <Formulario/>
+                </div>
+                
+                <div className="one-half column">
+                  2
+                </div>
+              </div>
+            )
+          }
+
         </div>
         
       </header>
